1a9189cd0fe80e387448c05749ddfc1978d58464
15321295560 <499801263@qq.cpm>
2020-06-09 15:40:13 +0800

    初始化项目

---
+++ DYZB/DYZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DYZB
+//
+//  Created by Glc on 2020/6/9.
+//  Copyright © 2020 Glc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
